fc2db7267dc421737a4af929ff2c872bdc929f92
PixelSergey <sergey.ichtchenko@gmail.com>
2024-02-23 14:49:55 +0200

    Change Chromium to Firefox

--- display/display.py
+++ display/display.py
@@ -8,12 +8,11 @@ driver = None
 def setup():
     global driver
 
-    service = webdriver.ChromeService(executable_path="/usr/lib/chromium-browser/chromedriver")
-    options = webdriver.ChromeOptions()
+    service = webdriver.FirefoxService(executable_path="/snap/bin/geckodriver")
+    options = webdriver.FirefoxOptions()
     options.add_argument("--start-maximized")
     options.add_argument("--kiosk")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Firefox(service=service, options=options)
 
     with open("config.json", "r") as f:
         raw_config = f.read()
---
+++ display/install.sh
@@ -0,0 +1,2 @@
+sudo snap install firefox
+pip3 install -r requirements.txt
