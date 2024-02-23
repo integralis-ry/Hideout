bd9b7d2b80fc8b61a79aab75410f361b020926cf
PixelSergey <sergey.ichtchenko@gmail.com>
2024-02-23 14:49:55 +0200

    Add HSL timetable view to transport display

--- transport/transport.py
+++ transport/transport.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import json
 
-service = webdriver.ChromeService(executable_path='/usr/lib/chromium-browser/chromedriver')
-driver = webdriver.Chrome(service=service)
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+
+def setup():
+    service = webdriver.ChromeService(executable_path='/usr/lib/chromium-browser/chromedriver')
+    driver = webdriver.Chrome(service=service)
+    return driver
+
+
+def main(driver):
+    driver.get("http://www.python.org")
+
+
+if __name__ == "__main__":
+    driver = setup()
+    main(driver)
