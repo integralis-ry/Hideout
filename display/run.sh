888ea039a3865106b7b65e1ae6d4ee1c34d4d1de
PixelSergey <sergey.ichtchenko@gmail.com>
2024-02-23 14:49:55 +0200

    Add sponsor and info pages

--- display/run.sh
+++ display/run.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/bash
 
-python3 display.py&
+cp info.html info_configured.html
+
+SSID=$(iwgetid -r)
+IP=$(hostname -I)
+WHOAMI=$(whoami)
+GITHUB='https://github.com/integralis-ry/Hideout'
+ESCAPED_GITHUB=$(printf '%s\n' "$GITHUB" | sed -e 's/[\/&]/\\&/g')
+EMAIL='it@integralis.fi'
+
+sed -i -e "s/{{SSID}}/$SSID/g" info_configured.html
+sed -i -e "s/{{IP}}/$IP/g" info_configured.html
+sed -i -e "s/{{WHOAMI}}/$WHOAMI/g" info_configured.html
+sed -i -e "s/{{GITHUB}}/$ESCAPED_GITHUB/g" info_configured.html
+sed -i -e "s/{{EMAIL}}/$EMAIL/g" info_configured.html
+
+python3 display.py
