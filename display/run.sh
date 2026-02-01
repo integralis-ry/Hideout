375484bebd71dd0b436eb888fdba3f668b3be82f
TheNushu <131345754+TheNushu@users.noreply.github.com>
2026-02-01 14:05:03 +0200

    Enhance run.sh to manage Python processes and reset Git
    Added commands to terminate existing Python processes and reset the Git repository before running scripts.

--- display/run.sh
+++ display/run.sh
@@ -7,6 +7,11 @@ DIR=$(pwd)
 VENV_PYTHON="$DIR/venv/bin/python3"
 MENU_DIR="/home/integralis/Hideout/unicafe_menu_creation"
 
+pkill -f python3
+sleep 1
+
+cd "$DIR"
+git reset --hard origin/main
 cp info.html info_configured.html
 cp config.json config_configured.json
 
@@ -29,9 +34,11 @@ export DISPLAY=:0
 
 echo "Running Menu Finder..."
 $VENV_PYTHON "$MENU_DIR/menu_finder.py"
+sleep 5
 
 echo "Starting Menu Creation App in background..."
 $VENV_PYTHON "$MENU_DIR/app.py" & 
+sleep 2
 
 echo "Launching Kiosk Display..."
 $VENV_PYTHON "$DIR/display.py"
