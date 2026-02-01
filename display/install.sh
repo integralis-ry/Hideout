6b3d6ba007dc15eaaece422d4e38d63350104d76
TheNushu <daniel.ioan.mlesnita.42@gmail.com>
2026-02-01 12:49:57 +0200

    Update shell scripts for system start-up

--- display/install.sh
+++ display/install.sh
@@ -1,2 +1,6 @@
-sudo snap install firefox
-pip3 install -r requirements.txt
+#!/bin/bash
+sudo apt update
+sudo apt install -y chromium-browser chromium-chromedriver x11-xserver-utils wireless-tools python3-pip python3-venv
+cd /home/integralis/Hideout/display
+python3 -m venv venv
+./venv/bin/pip install -r requirements.txt
--- display/run.sh
+++ display/run.sh
@@ -3,6 +3,10 @@
 # force last version of repo to be the github one
 git reset --hard origin/main
 
+DIR=$(pwd)
+VENV_PYTHON="$DIR/venv/bin/python3"
+MENU_DIR="/home/integralis/Hideout/unicafe_menu_creation"
+
 cp info.html info_configured.html
 cp config.json config_configured.json
 
@@ -12,9 +16,7 @@ WHOAMI=$(whoami)
 GITHUB='https://github.com/integralis-ry/Hideout'
 GITHUB_ESC=$(printf '%s\n' "$GITHUB" | sed -e 's/[\/&]/\\&/g')
 EMAIL='it@integralis.fi'
-DIR=$(pwd)
 DIR_ESC=$(printf '%s\n' "$DIR" | sed -e 's/[\/&]/\\&/g')
-
 
 sed -i -e "s/{{SSID}}/$SSID/g" info_configured.html
 sed -i -e "s/{{IP}}/$IP/g" info_configured.html
@@ -23,6 +25,13 @@ sed -i -e "s/{{GITHUB}}/$GITHUB_ESC/g" info_configured.html
 sed -i -e "s/{{EMAIL}}/$EMAIL/g" info_configured.html
 sed -i -e "s/{{DIR}}/$DIR_ESC/g" config_configured.json
 
-python3 ../unicafe_menu_creation/menu_finder.py
-python3 ../unicafe_menu_creation_app.py
-python3 display.py
+export DISPLAY=:0
+
+echo "Running Menu Finder..."
+$VENV_PYTHON "$MENU_DIR/menu_finder.py"
+
+echo "Starting Menu Creation App in background..."
+$VENV_PYTHON "$MENU_DIR/app.py" & 
+
+echo "Launching Kiosk Display..."
+$VENV_PYTHON "$DIR/display.py"
